4b7a8bf2c270b890a9cc8c37ebef54d695c185f4
Nicholas Gasior <nicholas@laatu.org>
2019-04-04 09:51:45 +0100

    Update doc

--- doc.go
+++ doc.go
@@ -1,9 +1,5 @@
-// 
-//
-
-
 /*
-Package go-cli is meant to make handling command line interface easier. 
+Package cli is meant to make handling command line interface easier. 
 
 You define commands with flags, attach a handler to it and package does all
 the parsing.
