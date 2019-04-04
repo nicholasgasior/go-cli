c882f5af317ef97785c3db74f7a5d115a059b972
Nicholas Gasior <nicholas@laatu.org>
2019-04-04 09:43:09 +0100

    Update doc

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /* 
-Package `nicholasgasior/go-cli` is meant to make handling command line interface easier. 
+Package go-cli is meant to make handling command line interface easier. 
 
 You define commands with flags, attach a handler to it and package does all
 the parsing.
