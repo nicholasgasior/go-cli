0948567a56836a3d542fb6f27f32f52f3d370886
Mikolaj Gasior <mg@f0x.pl>
2020-04-06 16:29:42 +0200

    Update docs

--- README.md
+++ README.md
@@ -27,46 +27,48 @@ Let's start with an example covering everything. First, let's create main
 func main() {
     myCLI      := cli.NewCLI("Example CLI", "Silly app", "Author <a@example.com>")
 
-    cmdHello   := myCLI.AddCmd("hello", "Prints out Hello", HelloHandler)
-    cmdJSONKey := myCLI.AddCmd("json_key", "Checks if JSON file has key", JSONKeyHandler)
+    cmdInit    := myCLI.AddCmd("init", "Initialises the project", InitHandler)
+    cmdStart   := myCLI.AddCmd("start", "Start the application", StartHandler)
 }
 ```
 
 Next, let's add flags to our commands:
 
 ```
-    cmdJSONKey.AddFlag("json-file", "JSON file", cli.CLIFlagTypePathFile | cli.CLIFlagMustExist | cli.CLIFlagRequired)
-    cmdJSONKey.AddFlag("json-key", "JSON key", cli.CLIFlagTypeString | cli.CLIFlagRequired)
+    cmdInit.AddFlag("template", "t", "filepath", "Path to template file", TypePathFile|MustExist|Required)
+    cmdInit.AddFlag("file-output", "o", "filepath", "Output to a specific file instead of stdout", TypePathFile)
+    cmdInit.AddFlag("number", "n", "int", "Number necessary for initialisation", TypeInt|Required)
 
-    cmdHello.AddFlag("language", "Language", cli.CLIFlagTypeString | cli.CLIFlagRequired)
-    cmdHello.AddFlag("color", "Add color", cli.CLIFlagTypeBool)
+    cmdStart.AddFlag("verbose", "v", "", "Verbose mode", TypeBool)
+    cmdStart.AddFlag("username", "u", "username", "Username", TypeAlphanumeric|AllowDots|AllowUnderscore|Required)
+    cmdStart.AddFlag("threshold", "", "1.5", "Threshold, default 1.5", TypeFloat)
 ```
 
 Third argument to `NewCLIFlag` is used to define what is the type of flag, is
 it required etc. It's an integer value and the following `const`s are
 available:
 
-* `CLIFlagTypePathFile` - flag is a path to a file (string);
-* `CLIFlagMustExist` - if added along with `CLIFlagTypePathFile` then path must exist;
-* `CLIFlagRequired` - flag is required (this does not work with bool flag);
-* `CLIFlagTypeString` - flag is a string;
-* `CLIFlagTypeBool` - flag is boolean and will have a value of "true" or "false".
+* `TypePathFile` - flag is a path to a file (string);
+* `MustExist` - if added along with `CLIFlagTypePathFile` then path must exist;
+* `Required` - flag is required (this does not work with bool flag);
+* `TypeString` - flag is a string;
+* `TypeBool` - flag is boolean and will have a value of "true" or "false";
+* `TypeAlphanumeric` - flag is string and have to match [0-9a-zA-Z]+.
+
+Check `cli_flag.go` for more information on flag types.
 
 Finally, let's create functions to handle our commands. In below code, you can
 see that method `Flag` on `CLI` instance (passed as first argument) can be
 used to get a flag value.
 
 ```
-func HelloHandler(c *cli.CLI) int {
-    fmt.Fprintf(os.Stdout, "Language: %s\n", c.Flag("language"))
-    fmt.Fprintf(os.Stdout, "Color: %s\n", c.Flag("color"))
-
+func InitHanlder(c *cli.CLI) int {
+    fmt.Fprintf(os.Stdout, "Template path: %s\n", c.Flag("template"))
     return 0
 }
 
-func JSONKeyHandler(c *cli.CLI) int {
-    fmt.Fprintf(os.Stdout, "JSON key: %s\n", c.Flag("json-key"))
-    fmt.Fprintf(os.Stdout, "JSON file: %s\n", c.Flag("json-file"))
+func StartHandler(c *cli.CLI) int {
+    fmt.Fprintf(os.Stdout, "Username: %s\n", c.Flag("username"))
     return 0
 }
 ```
--- cli_test.go
+++ cli_test.go
@@ -6,30 +6,30 @@ import (
 )
 
 func h(c *CLI) int {
-    return 0
+	return 0
 }
 
 func createCLI() *CLI {
 	c := NewCLI("Example CLI", "Silly app", "Author <a@example.com>")
 
-    cmd1 := c.AddCmd("command", "Prints out something", h)
-    cmd1.AddFlag("bool", "b", "", "Boolean flag", TypeBool)
-    cmd1.AddFlag("input", "i", "filepath", "Path to a file", TypePathFile|Required)
-    cmd1.AddFlag("title", "t", "title", "Title of the project", TypeString|Required)
-    cmd1.AddFlag("desc", "d", "description", "Description of the project", TypeString)
+	cmd1 := c.AddCmd("command", "Prints out something", h)
+	cmd1.AddFlag("bool", "b", "", "Boolean flag", TypeBool)
+	cmd1.AddFlag("input", "i", "filepath", "Path to a file", TypePathFile|Required)
+	cmd1.AddFlag("title", "t", "title", "Title of the project", TypeString|Required)
+	cmd1.AddFlag("desc", "d", "description", "Description of the project", TypeString)
 
-    cmd2 := c.AddCmd("anotherone", "Initialises project", h)
-    cmd2.AddFlag("int", "i", "int", "Integer flag", TypeInt|Required)
-    cmd2.AddFlag("float", "f", "float", "Float flag", TypeFloat|Required)
-    cmd2.AddFlag("anum", "", "alphanumeric", "Alphanumeric flag", TypeAlphanumeric|Required)
+	cmd2 := c.AddCmd("anotherone", "Initialises project", h)
+	cmd2.AddFlag("int", "i", "int", "Integer flag", TypeInt|Required)
+	cmd2.AddFlag("float", "f", "float", "Float flag", TypeFloat|Required)
+	cmd2.AddFlag("anum", "", "alphanumeric", "Alphanumeric flag", TypeAlphanumeric|Required)
 
-    cmd3 := c.AddCmd("three", "Third command that does something", h)
-    cmd3.AddFlag("many-ints", "i", "int,int,...", "Many integers comma-delimetered", TypeInt|AllowMany)
-    cmd3.AddFlag("many-floats", "f", "float;float;...", "Many floats semicolon-delimetered", TypeFloat|AllowMany|ManySeparatorSemiColon|Required)
-    cmd3.AddFlag("many-anums", "a", "anum:anum:...", "Many alphanumeric colon-delimetered", TypeAlphanumeric|AllowMany|ManySeparatorColon)
-    cmd3.AddFlag("more", "m", "alphanumeric+dot+uscore", "Alphanumeric flag with additional dots and underscore allowed", TypeAlphanumeric|AllowDots|AllowUnderscore|Required)
+	cmd3 := c.AddCmd("three", "Third command that does something", h)
+	cmd3.AddFlag("many-ints", "i", "int,int,...", "Many integers comma-delimetered", TypeInt|AllowMany)
+	cmd3.AddFlag("many-floats", "f", "float;float;...", "Many floats semicolon-delimetered", TypeFloat|AllowMany|ManySeparatorSemiColon|Required)
+	cmd3.AddFlag("many-anums", "a", "anum:anum:...", "Many alphanumeric colon-delimetered", TypeAlphanumeric|AllowMany|ManySeparatorColon)
+	cmd3.AddFlag("more", "m", "alphanumeric+dot+uscore", "Alphanumeric flag with additional dots and underscore allowed", TypeAlphanumeric|AllowDots|AllowUnderscore|Required)
 
-    return c
+	return c
 }
 
 func assertExitCode(t *testing.T, cli *CLI, a []string, c int) {
@@ -48,26 +48,26 @@ func TestFlags(t *testing.T) {
 
 	t.Run("exit with code 0 when no flags or help", func(t *testing.T) {
 		assertExitCode(t, c, []string{"test"}, 0)
-        assertExitCode(t, c, []string{"test", "-h"}, 0)
-        assertExitCode(t, c, []string{"test", "--help"}, 0)
-        assertExitCode(t, c, []string{"test", "command", "-h"}, 0)
-        assertExitCode(t, c, []string{"test", "command", "--help"}, 0)
+		assertExitCode(t, c, []string{"test", "-h"}, 0)
+		assertExitCode(t, c, []string{"test", "--help"}, 0)
+		assertExitCode(t, c, []string{"test", "command", "-h"}, 0)
+		assertExitCode(t, c, []string{"test", "command", "--help"}, 0)
 	})
 
-    t.Run("exit with code 1 when required flag is missing", func(t *testing.T) {
-        assertExitCode(t, c, []string{"test", "command", "-b", "-t", "title", "-d", "desc"}, 1)
-        assertExitCode(t, c, []string{"test", "command", "-i", "cli_test.go"}, 1)
-        assertExitCode(t, c, []string{"test", "anotherone"}, 1)
-        assertExitCode(t, c, []string{"test", "three", "-i", "1,2,3"}, 1)
-    })
+	t.Run("exit with code 1 when required flag is missing", func(t *testing.T) {
+		assertExitCode(t, c, []string{"test", "command", "-b", "-t", "title", "-d", "desc"}, 1)
+		assertExitCode(t, c, []string{"test", "command", "-i", "cli_test.go"}, 1)
+		assertExitCode(t, c, []string{"test", "anotherone"}, 1)
+		assertExitCode(t, c, []string{"test", "three", "-i", "1,2,3"}, 1)
+	})
 
-    t.Run("exit with code 1 when value is invalid", func(t *testing.T) {
-        assertExitCode(t, c, []string{"test", "command", "-i", "nonexistingfile", "-t", "title"}, 1)
-        assertExitCode(t, c, []string{"test", "anotherone", "--int", "aaaa", "--float", "123.12", "--anum", "validvalue"}, 1)
-        assertExitCode(t, c, []string{"test", "anotherone", "--int", "123", "--float", "123", "--anum", "validvalue"}, 1)
-        assertExitCode(t, c, []string{"test", "anotherone", "--int", "123", "--float", "123.12", "--anum", "^^4443####"}, 1)
-        assertExitCode(t, c, []string{"test", "three", "-i", "aasd,asda", "-f", "12.33", "-a", "user1", "-m", "user.1"}, 1)
-        assertExitCode(t, c, []string{"test", "three", "-i", "1,2,3", "-f", "12,33", "-a", "user1", "-m", "user.1"}, 1)
-        assertExitCode(t, c, []string{"test", "three", "-i", "1,2,3", "-f", "23.23,24.24,25.25", "-a", "user1.", "-m", "user_1"}, 1)
-    })
+	t.Run("exit with code 1 when value is invalid", func(t *testing.T) {
+		assertExitCode(t, c, []string{"test", "command", "-i", "nonexistingfile", "-t", "title"}, 1)
+		assertExitCode(t, c, []string{"test", "anotherone", "--int", "aaaa", "--float", "123.12", "--anum", "validvalue"}, 1)
+		assertExitCode(t, c, []string{"test", "anotherone", "--int", "123", "--float", "123", "--anum", "validvalue"}, 1)
+		assertExitCode(t, c, []string{"test", "anotherone", "--int", "123", "--float", "123.12", "--anum", "^^4443####"}, 1)
+		assertExitCode(t, c, []string{"test", "three", "-i", "aasd,asda", "-f", "12.33", "-a", "user1", "-m", "user.1"}, 1)
+		assertExitCode(t, c, []string{"test", "three", "-i", "1,2,3", "-f", "12,33", "-a", "user1", "-m", "user.1"}, 1)
+		assertExitCode(t, c, []string{"test", "three", "-i", "1,2,3", "-f", "23.23,24.24,25.25", "-a", "user1.", "-m", "user_1"}, 1)
+	})
 }
